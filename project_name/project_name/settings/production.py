2f0102c88828000e9a92cae0b81ea1c2a92471ea
Jonathon W. Marshall <jonathon@bnotions.com>
2013-09-10 17:20:40 -0400

    import cache_settings block

--- project_name/project_name/settings/production.py
+++ project_name/project_name/settings/production.py
@@ -69,12 +69,10 @@ except ImportError:
 
 ########## CACHE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'application_cache'
-    }
-}
+try:
+    from cache_settings import *
+except ImportError:
+    pass
 ########## END CACHE CONFIGURATION
 
 
